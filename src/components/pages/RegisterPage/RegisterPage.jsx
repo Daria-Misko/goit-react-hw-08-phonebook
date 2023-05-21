d2e3254b18e3e671bd650248fd3002bcf45f80e0
Daria-Misko <miskodaria.mail@gmail.com>
2023-05-21 10:23:19 +0300

    add register page

--- src/components/pages/RegisterPage/RegisterPage.jsx
+++ src/components/pages/RegisterPage/RegisterPage.jsx
@@ -1,40 +1,40 @@
-// // import { RegisterForm } from 'components/RegisterForm/RegisterForm';
-// import { useDispatch } from 'react-redux';
-// import { register } from 'redux/auth/authOperations';
-// import { Btn, Form, Input, Label } from './RegisterPage.styles';
-// // import { Helmet } from 'react-helmet';
+// import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/authOperations';
+import { Btn, Form, Input, Label } from './RegisterPage.styles';
+// import { Helmet } from 'react-helmet';
 
-// export default function RegisterPage() {
-//   const dispatch = useDispatch();
+export default function RegisterPage() {
+  const dispatch = useDispatch();
 
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     const form = e.currentTarget;
-//     dispatch(
-//       register({
-//         name: form.elements.name.value,
-//         email: form.elements.email.value,
-//         password: form.elements.password.value,
-//       })
-//     );
-//     form.reset();
-//   };
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
+  };
 
-//   return (
-//     <Form onSubmit={handleSubmit} autoComplete="off">
-//       <Label>
-//         Username
-//         <Input type="text" name="name" />
-//       </Label>
-//       <Label>
-//         Email
-//         <Input type="email" name="email" />
-//       </Label>
-//       <Label>
-//         Password
-//         <Input type="password" name="password" />
-//       </Label>
-//       <Btn type="submit">Register</Btn>
-//     </Form>
-//   );
-// }
+  return (
+    <Form onSubmit={handleSubmit} autoComplete="off">
+      <Label>
+        Username
+        <Input type="text" name="name" />
+      </Label>
+      <Label>
+        Email
+        <Input type="email" name="email" />
+      </Label>
+      <Label>
+        Password
+        <Input type="password" name="password" />
+      </Label>
+      <Btn type="submit">Register</Btn>
+    </Form>
+  );
+}
